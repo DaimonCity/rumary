ea00c2b5491c81fbb20bef8a09fee6346e920e1a
daimon <dsamojlov04@gmail.com>
2026-07-02 00:24:38 +0700

    use Arc was added

--- crates/rumary-api/src/state.rs
+++ crates/rumary-api/src/state.rs
@@ -1,12 +1,12 @@
-use std::sync::Arc;
 use crate::error::AppError;
 use crate::service::totp::TotpService;
 use crate::services::{AuthProvider, UserProfileProvider};
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub auth: Arc<dyn AuthProvider<Error=AppError>>,
-    pub user_profile: Arc<dyn UserProfileProvider<Error=AppError>>,
+    pub auth: Arc<dyn AuthProvider<Error = AppError>>,
+    pub user_profile: Arc<dyn UserProfileProvider<Error = AppError>>,
     pub totp: Arc<TotpService>,
     pub secure_cookies: bool,
 }
